ea5115b09314924c2dd8618fdded2eb328464e4c
ccc <gcnice@163.com>
2018-04-18 15:10:56 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'articles#index'
+
   resources :articles do
   resources :comments
   end
-
-  get '/',to: 'articles#index'
+  
+  root to: 'articles#index'
+  get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
