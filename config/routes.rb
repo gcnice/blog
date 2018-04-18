5476a2493c882b5ecb74d10dad5e4ddbb6fe1fa0
ccc <gcnice@163.com>
2018-04-18 11:32:13 +0800

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   end
 
   root 'articles#index'
+  get '/',to: 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
